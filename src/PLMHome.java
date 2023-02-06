d8db616f327f99be1577d0a35ff025e769eb73ca
Benoit THOURY <benoit.thoury@gmail.com>
2023-02-06 08:57:12 +0100

    monday commit

--- src/PLMHome.java
+++ src/PLMHome.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -15,7 +16,7 @@ import java.util.Scanner;
  *
  */
 public class PLMHome {
-	
+
 	/**
 	 * 
 	 * 
@@ -25,64 +26,70 @@ public class PLMHome {
 
 		Scanner scanner = new Scanner(System.in);
 
-		List<HashMap<String, String>> mesAvions = Arrays.asList(
-				creerAvion("A330", "PLM_AIRBUSIN_SERVICE", "Passenger"),
+		List<HashMap<String, String>> mesAvions = Arrays.asList(creerAvion("A330", "PLM_AIRBUSIN_SERVICE", "Passenger"),
 				creerAvion("A400M", "PLM_AIRBUSIN_DEFINITTION", "Cargo"),
 				creerAvion("A330NEO", "PLM_AIRBUSIN_SERVICE", "Passenger"),
-				creerAvion("A300", "PLM_AIRBUSIN_SERVICE", "Passenger]"),
+				creerAvion("A300", "PLM_AIRBUSIN_SERVICE", "Passenger"),
 				creerAvion("A380", "PLM_AIRBUSIN_SERVICE", "Passenger"));
-
-		System.out.println(
-				"Bienvenue dans l'application de gestion du cycle de vie d'avions AIRBUS\nFaites votre choix dans le menu, saisissez le chiffre correspondant :\n\n1 : Afficher tous les avions\n2 : Afficher tous les avions contenant un mot clé dans le programme\n3 : Afficher ou suprimer une pièce pour un avion donné\n4 : Afficher un avion avec les infos détaillées de chaque pièces\n5 : quitter l'application");
-
-		String choice = scanner.nextLine();
-		System.out.println(choice);
-		int choice1 = Integer.parseInt(choice);
-		int count = 1;
 		
-		
-		switch (choice1) {
-		case 1:
+	//	Avion toto = new Avion("A380", "PLM_AIRBUSIN_SERVICE", "Passenger", null);
 
-			
-			for (HashMap<String, String> monAvion : mesAvions) {
-				System.out.println(count + "= [" + monAvion.get("id") + " - " + monAvion.get("program") + " - "
-						+ monAvion.get("phase") + "]");
-				count++;
+		do {
+			System.out.println("Bienvenue dans l'application de gestion du cycle de vie d'avions AIRBUS\n"
+					+ "Faites votre choix dans le menu, saisissez le chiffre correspondant :\n\n"
+					+ "1 : Afficher tous les avions\n"
+					+ "2 : Afficher tous les avions contenant un mot clé dans le programme\n"
+					+ "3 : Afficher ou suprimer une pièce pour un avion donné\n"
+					+ "4 : Afficher un avion avec les infos détaillées de chaque pièces\n"
+					+ "5 : Quitter l'application");
+			String valeurLue = scanner.next();
+
+			if (isNumber(valeurLue)) {
+				int choix = Integer.parseInt(valeurLue);
+
+				switch (choix) {
+				case 1:
+					int compt = 1;
+					for (HashMap<String, String> monAvion : mesAvions) {
+						System.out.println(compt + "= [ " + monAvion.get("id") + " " + monAvion.get("program") + " " + monAvion.get("phase") + " ]");
+						compt++;
+					}
+					break;
+				case 2:
+					System.out.println("Veuillez saisir un nom d'avion: ");
+					valeurLue = scanner.next();
+					boolean isAvion = false;
+					compt = 1;
+					System.out.println("toto");
+					for (HashMap<String, String> monAvion : mesAvions) {
+						if (monAvion.get("id").contains(valeurLue)) {
+							isAvion = true;
+							System.out.println(compt + "= [ " + monAvion.get("id") + " "
+									+ monAvion.get("program") + " " + monAvion.get("phase")  + " ]");
+							compt++;
+						}
+					}
+					if (!isAvion) {
+						System.out.println("La valeur saisie n'est pas un avion.");
+					}
+					break;
+				case 3:
+
+					break;
+				case 4:
+
+					break;
+				case 5:
+
+					break;
+				default:
+					System.out.println("Option inexistante.");
+					break;
+				}
+			} else {
+				System.out.println("Ce n'est pas un nombre.");
 			}
-
-			break;
-		case 2:
-
-			System.out.println("Merci de renseigner un mot clé d'un avion. \nExemple: A300");
-
-			String choiceAirplane = scanner.nextLine();
-			
-			for (HashMap<String, String> monAvion : mesAvions) {
-				
-				int index = monAvion.get("id").indexOf(choiceAirplane.toUpperCase());
-				
-				if( index == 1 ) {
-					System.out.println(count + "= [" + monAvion.get("id") + " - " + monAvion.get("program") + " - "
-							+ monAvion.get("phase") + "]");
-				}		
-				count++;
-			}
-		
-
-			break;
-		case 3:
-
-			break;
-		case 4:
-
-			break;
-		case 5:
-		default:
-			System.out.println("Choix incorrect");
-			break;
-		}
-
+		} while (onContinue());
 	}
 
 	/**
@@ -102,20 +109,24 @@ public class PLMHome {
 		return airplane;
 	}
 
-	/**
-	 * 
-	 * @param
-	 * @return false or true
-	 */
-	private static boolean aircraftSearch(int index) {
-		boolean faut;
-		if (index == -1) {
-			faut = false;
-			return faut;
-		} else {
-			faut = true;
-			return faut;
+	private static boolean isNumber(String monString) {
+		char[] monCharArray = monString.toCharArray();
+		for (char monChar : monCharArray) {
+			if (Character.isDigit(monChar) == false) {
+				return false;
+			}
 		}
+		return true;
+	}
+
+	private static boolean onContinue() {
+		Scanner scanner = new Scanner(System.in);
+		char valeurLue = ' ';
+		do {
+			System.out.println("Voulez-vous réessayer ? (O/N)");
+			valeurLue = scanner.next().charAt(0);
+		} while (valeurLue != 'O' && valeurLue != 'N');
+		return valeurLue == 'O';
 	}
 
 }
